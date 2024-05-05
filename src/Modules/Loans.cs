e94fdae6be881e637b99bcb5c6526694da2be852
Qoqqi <sazykin.georgii@gmail.com>
2024-05-05 13:47:33 +0300

    feat: Добавлен модуль Loans, изменяющий набор доступных игроку займов

---
+++ src/Modules/Loans.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace QMarketPlugin.Modules;
+
+public static class Loans {
+
+    private static readonly IDictionary<int, Action<BankCreditSO>> LoanFillers =
+            new Dictionary<int, Action<BankCreditSO>>();
+
+    private static readonly IDictionary<int, BankCreditSO> LoanInstances = new Dictionary<int, BankCreditSO>();
+
+    private static readonly IDictionary<int, string> LoanTitles = new Dictionary<int, string>();
+
+    static Loans() {
+        AddLoan(100, 0, 0, "Занять у родственников");
+        AddLoan(500, 0, 10, "Занять у друга");
+        AddLoan(1500, 0, 25, "Занять у близкого друга");
+        AddLoan(2500, 0, 40, "Занять у знакомого бизнесмена");
+
+        AddLoan(300, 0.1f, 0, "Занять у местной группировки");
+        AddLoan(2000, 0.15f, 15, "Средний займ у группировки");
+        AddLoan(4000, 0.20f, 30, "Большой займ у группировки");
+        AddLoan(8000, 0.25f, 45, "Крупный займ у группировки");
+
+        AddLoan(1000, 0.01f, 5, "Малый кредит");
+        AddLoan(2500, 0.02f, 20, "Средний кредит");
+        AddLoan(5000, 0.03f, 35, "Большой кредит");
+        AddLoan(10000, 0.04f, 50, "Профессиональный кредит");
+    }
+
+    private static void SetupLoans(IEnumerable<BankCreditSO> existingLoans) {
+        var loans = new List<BankCreditSO>(existingLoans);
+
+        CreateMissingLoans(loans);
+        SetupLoanInstances(loans);
+    }
+
+    private static void CreateMissingLoans(List<BankCreditSO> loans) {
+        foreach (var loanId in LoanFillers.Keys) {
+            if (!LoanExists(loans, loanId)) {
+                loans.Add(CreateExtraLoan(loanId));
+            }
+        }
+    }
+
+    private static void SetupLoanInstances(List<BankCreditSO> loans) {
+        foreach (var loan in loans) {
+            SetupLoan(loan);
+            LoanInstances[loan.ID] = loan;
+        }
+    }
+
+    private static void SetupLoan(BankCreditSO loan) {
+        LoanFillers[loan.ID](loan);
+    }
+
+    private static void AddMissingLoans(List<BankCreditSO> loans) {
+        foreach (var loanId in LoanFillers.Keys) {
+            EnsureLoanExists(loans, loanId);
+        }
+    }
+
+    private static void EnsureLoanExists(List<BankCreditSO> loans, int loanId) {
+        if (!LoanExists(loans, loanId) && CanAddExtraLoan(loanId)) {
+            AddExtraLoan(loans, loanId);
+        }
+    }
+
+    private static void AddMissingLoanDatas(ICollection<LoanData> loanDatas, IReadOnlyList<BankCreditSO> loans) {
+        while (loanDatas.Count < loans.Count) {
+            var loan = loans[loanDatas.Count];
+
+            loanDatas.Add(new LoanData {
+                    LoanID = loan.ID
+            });
+        }
+    }
+
+    private static void AddLocalizationKeys(IDictionary<int, string> localizedLoanNames) {
+        foreach (var entry in LoanTitles) {
+            localizedLoanNames[entry.Key] = entry.Value;
+        }
+    }
+
+    private static void AddLoan(int sum, float dailyPercent, int requiredLevel, string title) {
+        var loanId = LoanFillers.Count + 1;
+
+        LoanTitles.Add(loanId, title);
+        LoanFillers.Add(loanId, loan => {
+            loan.Amount = sum;
+            loan.DailyInterestPercent = dailyPercent;
+            loan.RequiredPlayerLevel = requiredLevel;
+        });
+    }
+
+    private static bool LoanExists(List<BankCreditSO> loans, int id) {
+        return loans.Find(loan => loan.ID == id);
+    }
+
+    private static bool CanAddExtraLoan(int loanId) {
+        return LoanFillers.ContainsKey(loanId);
+    }
+
+    private static void AddExtraLoan(ICollection<BankCreditSO> loans, int loanId) {
+        loans.Add(LoanInstances[loanId]);
+    }
+
+    private static BankCreditSO CreateExtraLoan(int loanId) {
+        var loan = ScriptableObject.CreateInstance<BankCreditSO>();
+
+        loan.ID = loanId;
+
+        return loan;
+    }
+
+    private static void SetupLoansTab(LoansTab tab) {
+        var loansGameObject = tab.transform.Find("Loans").gameObject;
+        var gridLayout = loansGameObject.GetComponent<GridLayoutGroup>();
+
+        gridLayout.padding.left = 6;
+        gridLayout.padding.top = -10;
+        gridLayout.spacing -= new Vector2(60f, 40f);
+    }
+
+    private static void SetupLoanItem(LoanItem loanItem) {
+        var rectTransform = loanItem.GetComponent<RectTransform>();
+
+        rectTransform.localScale = new Vector3(0.75f, 0.75f, 1f);
+    }
+
+    public static class Patches {
+
+        [HarmonyPatch(typeof(LoansTab), "Start")]
+        [HarmonyPostfix]
+        private static void Setup(LoansTab __instance) {
+            SetupLoansTab(__instance);
+        }
+
+        [HarmonyPatch(typeof(LoanItem), "Setup")]
+        [HarmonyPostfix]
+        private static void Setup(LoanItem __instance) {
+            SetupLoanItem(__instance);
+        }
+
+        [HarmonyPatch(typeof(LocalizationManager), "Awake")]
+        [HarmonyPrefix]
+        private static void Awake() {
+            SetupLoans(IDManager.Instance.Loans);
+        }
+
+        [HarmonyPatch(typeof(LocalizationManager), "UpdateLocalization")]
+        [HarmonyPostfix]
+        private static void UpdateLocalization(Dictionary<int, string> ___m_LocalizedLoanNames) {
+            AddLocalizationKeys(___m_LocalizedLoanNames);
+        }
+
+        [HarmonyPatch(typeof(IDManager), "BankCreditSO")]
+        [HarmonyPrefix]
+        private static void BankCreditSo(int __0, List<BankCreditSO> ___m_BankCredits) {
+            EnsureLoanExists(___m_BankCredits, __0);
+        }
+
+        [HarmonyPatch(typeof(BankManager), "Start")]
+        [HarmonyPrefix]
+        private static void Setup(List<BankCreditSO> ___m_Loans) {
+            AddMissingLoans(___m_Loans);
+        }
+
+        [HarmonyPatch(typeof(BankManager), "LoadLoanDatas")]
+        [HarmonyPostfix]
+        private static void LoadLoanDatas(List<LoanData> ___m_LoanDatas, List<BankCreditSO> ___m_Loans) {
+            AddMissingLoanDatas(___m_LoanDatas, ___m_Loans);
+        }
+    }
+}
