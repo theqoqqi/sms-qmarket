30db95e713d75e8f93ac29f156eca5249a169c8c
Qoqqi <sazykin.georgii@gmail.com>
2024-05-06 13:18:46 +0300

    refactor: Класс DailyStatistics.Patches вынесен в отдельный файл DailyStatistics.Patches.cs

---
+++ src/Modules/DailyStatistics.Patches.cs
@@ -0,0 +1,43 @@
+﻿using HarmonyLib;
+using TMPro;
+
+namespace QMarketPlugin.Modules;
+
+public static partial class DailyStatistics {
+    public static class Patches {
+
+        [HarmonyPatch(typeof(DailyStatisticsManager), "DailyProfit", MethodType.Getter)]
+        [HarmonyPrefix]
+        private static void DailyProfit_Fix(
+                DailyStatisticsManager __instance,
+                out float __result,
+                out bool __runOriginal
+        ) {
+            __result = GetDailyProfit(__instance);
+            __runOriginal = false;
+        }
+
+        [HarmonyPatch(typeof(DailyStatisticsScreen), "Start")]
+        [HarmonyPostfix]
+        private static void Start(
+                DailyStatisticsScreen __instance,
+                DailyStatisticsScreenAnimation ___m_ScreenAnimation,
+                TMP_Text ___m_BillsText,
+                TMP_Text ___m_BalanceText
+        ) {
+            SetupGui(__instance, ___m_ScreenAnimation, ___m_BillsText, ___m_BalanceText);
+        }
+
+        [HarmonyPatch(typeof(DailyStatisticsScreen), "UpdateDailyStatistics")]
+        [HarmonyPostfix]
+        private static void UpdateDailyStatistics() {
+            UpdateGui();
+        }
+
+        [HarmonyPatch(typeof(DailyStatisticsScreen), "OnDayCycleDisabled")]
+        [HarmonyPostfix]
+        private static void OnDayCycleDisabled() {
+            RemoveGui();
+        }
+    }
+}
--- src/Modules/DailyStatistics.cs
+++ src/Modules/DailyStatistics.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using HarmonyLib;
 using QMarketPlugin.Patches;
 using QMarketPlugin.Utils;
 using TMPro;
@@ -9,7 +8,7 @@ using UnityEngine.Localization.Components;
 
 namespace QMarketPlugin.Modules;
 
-public static class DailyStatistics {
+public static partial class DailyStatistics {
 
     private static TMP_Text billsText;
 
@@ -103,42 +102,5 @@ public static class DailyStatistics {
         Object.Destroy(dailyWageLine.GetComponent<LocalizeStringEvent>());
 
         return dailyWageText;
-    }
-
-    public static class Patches {
-
-        [HarmonyPatch(typeof(DailyStatisticsManager), "DailyProfit", MethodType.Getter)]
-        [HarmonyPrefix]
-        private static void DailyProfit_Fix(
-                DailyStatisticsManager __instance,
-                out float __result,
-                out bool __runOriginal
-        ) {
-            __result = GetDailyProfit(__instance);
-            __runOriginal = false;
-        }
-
-        [HarmonyPatch(typeof(DailyStatisticsScreen), "Start")]
-        [HarmonyPostfix]
-        private static void Start(
-                DailyStatisticsScreen __instance,
-                DailyStatisticsScreenAnimation ___m_ScreenAnimation,
-                TMP_Text ___m_BillsText,
-                TMP_Text ___m_BalanceText
-        ) {
-            SetupGui(__instance, ___m_ScreenAnimation, ___m_BillsText, ___m_BalanceText);
-        }
-
-        [HarmonyPatch(typeof(DailyStatisticsScreen), "UpdateDailyStatistics")]
-        [HarmonyPostfix]
-        private static void UpdateDailyStatistics() {
-            UpdateGui();
-        }
-
-        [HarmonyPatch(typeof(DailyStatisticsScreen), "OnDayCycleDisabled")]
-        [HarmonyPostfix]
-        private static void OnDayCycleDisabled() {
-            RemoveGui();
-        }
     }
 }
