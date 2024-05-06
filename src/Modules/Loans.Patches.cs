1a673f1bb0f243ddc64da72f70869e2ceb27d52b
Qoqqi <sazykin.georgii@gmail.com>
2024-05-06 13:24:32 +0300

    refactor: Класс Loans.Patches вынесен в отдельный файл Loans.Patches.cs

---
+++ src/Modules/Loans.Patches.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using TMPro;
+using UnityEngine.UI;
+
+namespace QMarketPlugin.Modules;
+
+public static partial class Loans {
+    public static class Patches {
+
+        [HarmonyPatch(typeof(LoansTab), "Start")]
+        [HarmonyPostfix]
+        private static void Setup(LoansTab __instance) {
+            SetupLoansTab(__instance);
+        }
+
+        [HarmonyPatch(typeof(LoanItem), "Setup")]
+        [HarmonyPostfix]
+        private static void Setup(LoanItem __instance) {
+            SetupLoanItem(__instance);
+        }
+        
+        [HarmonyPatch(typeof(LoanItem), "UpdateAvailableLayoutUI")]
+        [HarmonyPostfix]
+        private static void UpdateAvailableLayoutUI(
+                BankCreditSO ___m_Loan,
+                TMP_Text ___m_DailyInterestText,
+                Button ___m_LoanButton
+        ) {
+            UpdateLoanAvailableLayout(___m_Loan, ___m_DailyInterestText, ___m_LoanButton);
+        }
+
+        [HarmonyPatch(typeof(LocalizationManager), "Awake")]
+        [HarmonyPrefix]
+        private static void Awake() {
+            SetupLoans(IDManager.Instance.Loans);
+        }
+
+        [HarmonyPatch(typeof(LocalizationManager), "UpdateLocalization")]
+        [HarmonyPostfix]
+        private static void UpdateLocalization(Dictionary<int, string> ___m_LocalizedLoanNames) {
+            AddLocalizationKeys(___m_LocalizedLoanNames);
+        }
+
+        [HarmonyPatch(typeof(IDManager), "BankCreditSO")]
+        [HarmonyPrefix]
+        private static void BankCreditSo(List<BankCreditSO> ___m_BankCredits) {
+            AddMissingLoans(___m_BankCredits);
+        }
+
+        [HarmonyPatch(typeof(BankManager), "Start")]
+        [HarmonyPrefix]
+        private static void Setup(List<BankCreditSO> ___m_Loans) {
+            AddMissingLoans(___m_Loans);
+            AddLoanListeners();
+        }
+
+        [HarmonyPatch(typeof(BankManager), "LoadLoanDatas")]
+        [HarmonyPostfix]
+        private static void LoadLoanDatas(List<LoanData> ___m_LoanDatas, List<BankCreditSO> ___m_Loans) {
+            AddMissingLoanDatas(___m_LoanDatas, ___m_Loans);
+        }
+    }
+}
--- src/Modules/Loans.cs
+++ src/Modules/Loans.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using HarmonyLib;
 using QMarketPlugin.Utils;
 using TMPro;
 using UnityEngine;
@@ -10,7 +9,7 @@ using Object = UnityEngine.Object;
 
 namespace QMarketPlugin.Modules;
 
-public static class Loans {
+public static partial class Loans {
 
     private static readonly IDictionary<int, LoanInfo> LoanInfos = new Dictionary<int, LoanInfo>();
 
@@ -195,61 +194,5 @@ public static class Loans {
         Private,
         Criminals,
         Bank
-    }
-
-    public static class Patches {
-
-        [HarmonyPatch(typeof(LoansTab), "Start")]
-        [HarmonyPostfix]
-        private static void Setup(LoansTab __instance) {
-            SetupLoansTab(__instance);
-        }
-
-        [HarmonyPatch(typeof(LoanItem), "Setup")]
-        [HarmonyPostfix]
-        private static void Setup(LoanItem __instance) {
-            SetupLoanItem(__instance);
-        }
-        
-        [HarmonyPatch(typeof(LoanItem), "UpdateAvailableLayoutUI")]
-        [HarmonyPostfix]
-        private static void UpdateAvailableLayoutUI(
-                BankCreditSO ___m_Loan,
-                TMP_Text ___m_DailyInterestText,
-                Button ___m_LoanButton
-        ) {
-            UpdateLoanAvailableLayout(___m_Loan, ___m_DailyInterestText, ___m_LoanButton);
-        }
-
-        [HarmonyPatch(typeof(LocalizationManager), "Awake")]
-        [HarmonyPrefix]
-        private static void Awake() {
-            SetupLoans(IDManager.Instance.Loans);
-        }
-
-        [HarmonyPatch(typeof(LocalizationManager), "UpdateLocalization")]
-        [HarmonyPostfix]
-        private static void UpdateLocalization(Dictionary<int, string> ___m_LocalizedLoanNames) {
-            AddLocalizationKeys(___m_LocalizedLoanNames);
-        }
-
-        [HarmonyPatch(typeof(IDManager), "BankCreditSO")]
-        [HarmonyPrefix]
-        private static void BankCreditSo(List<BankCreditSO> ___m_BankCredits) {
-            AddMissingLoans(___m_BankCredits);
-        }
-
-        [HarmonyPatch(typeof(BankManager), "Start")]
-        [HarmonyPrefix]
-        private static void Setup(List<BankCreditSO> ___m_Loans) {
-            AddMissingLoans(___m_Loans);
-            AddLoanListeners();
-        }
-
-        [HarmonyPatch(typeof(BankManager), "LoadLoanDatas")]
-        [HarmonyPostfix]
-        private static void LoadLoanDatas(List<LoanData> ___m_LoanDatas, List<BankCreditSO> ___m_Loans) {
-            AddMissingLoanDatas(___m_LoanDatas, ___m_Loans);
-        }
     }
 }
