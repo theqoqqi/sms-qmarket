97b1c17b3a0067063c4bc108247395c7d72eed59
Qoqqi <sazykin.georgii@gmail.com>
2024-05-06 13:35:40 +0300

    refactor: Методы для работы с интерфейсом из класса Loans вынесены в отдельный файл Loans.Gui.cs

---
+++ src/Modules/Loans.Gui.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using QMarketPlugin.Utils;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Object = UnityEngine.Object;
+
+namespace QMarketPlugin.Modules;
+
+public static partial class Loans {
+    private static void AddLocalizationKeys(IDictionary<int, string> localizedLoanNames) {
+        foreach (var entry in LoanInfos) {
+            localizedLoanNames[entry.Key] = entry.Value.Title;
+        }
+    }
+
+    private static void SetupLoansTab(LoansTab tab) {
+        var loansGameObject = tab.transform.Find("Loans").gameObject;
+        var gridLayout = loansGameObject.GetComponent<GridLayoutGroup>();
+
+        gridLayout.padding.left = 6;
+        gridLayout.padding.top = -10;
+        gridLayout.spacing -= new Vector2(60f, 40f);
+    }
+
+    private static void SetupLoanItem(LoanItem loanItem) {
+        var rectTransform = loanItem.GetComponent<RectTransform>();
+
+        rectTransform.localScale = new Vector3(0.75f, 0.75f, 1f);
+    }
+
+    private static void UpdateLoanAvailableLayout(
+            BankCreditSO loan,
+            TMP_Text dailyInterestText,
+            Button activateButton
+    ) {
+        dailyInterestText.text = $"{loan.DailyInterestPercent * 100}%";
+
+        SetLoanButtonActive(activateButton, CanTakeLoan(loan));
+    }
+
+    private static void SetLoanButtonActive(Button button, bool isActive) {
+        var activeColor = new Color(0.063f, 0.708f, 0.3f);
+        var lockedColor = new Color(0.708f, 0.063f, 0.3f);
+        var color = isActive ? activeColor : lockedColor;
+
+        button.interactable = isActive;
+        button.GetComponent<Image>().color = color;
+    }
+
+    private static void UpdateLoanUis() {
+        var loanItems = Object.FindObjectsOfType<LoanItem>();
+
+        foreach (var loanItem in loanItems) {
+            var method = ReflectionUtils.GetNonPublicMethod(loanItem, "UpdateAvailableLayoutUI");
+
+            method.Invoke(loanItem, Array.Empty<object>());
+        }
+    }
+}
--- src/Modules/Loans.cs
+++ src/Modules/Loans.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using QMarketPlugin.Utils;
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
-using Object = UnityEngine.Object;
 
 namespace QMarketPlugin.Modules;
 
@@ -88,12 +83,6 @@ public static partial class Loans {
         }
     }
 
-    private static void AddLocalizationKeys(IDictionary<int, string> localizedLoanNames) {
-        foreach (var entry in LoanInfos) {
-            localizedLoanNames[entry.Key] = entry.Value.Title;
-        }
-    }
-
     private static bool LoanExists(List<BankCreditSO> loans, int id) {
         return loans.Find(loan => loan.ID == id);
     }
@@ -106,40 +95,6 @@ public static partial class Loans {
         loans.Add(LoanInfos[loanId].Instance);
     }
 
-    private static void SetupLoansTab(LoansTab tab) {
-        var loansGameObject = tab.transform.Find("Loans").gameObject;
-        var gridLayout = loansGameObject.GetComponent<GridLayoutGroup>();
-
-        gridLayout.padding.left = 6;
-        gridLayout.padding.top = -10;
-        gridLayout.spacing -= new Vector2(60f, 40f);
-    }
-
-    private static void SetupLoanItem(LoanItem loanItem) {
-        var rectTransform = loanItem.GetComponent<RectTransform>();
-
-        rectTransform.localScale = new Vector3(0.75f, 0.75f, 1f);
-    }
-    
-    private static void UpdateLoanAvailableLayout(
-            BankCreditSO loan,
-            TMP_Text dailyInterestText,
-            Button activateButton
-    ) {
-        dailyInterestText.text = $"{loan.DailyInterestPercent * 100}%";
-
-        SetLoanButtonActive(activateButton, CanTakeLoan(loan));
-    }
-
-    private static void SetLoanButtonActive(Button button, bool isActive) {
-        var activeColor = new Color(0.063f, 0.708f, 0.3f);
-        var lockedColor = new Color(0.708f, 0.063f, 0.3f);
-        var color = isActive ? activeColor : lockedColor;
-
-        button.interactable = isActive;
-        button.GetComponent<Image>().color = color;
-    }
-    
     private static bool CanTakeLoan(BankCreditSO loan) {
         return !BankManager.Instance.Loans
                 .Any(data => data.Taken && IsSameSource(loan, data));
@@ -154,13 +109,4 @@ public static partial class Loans {
         BankManager.Instance.onCompletedLoan += _ => UpdateLoanUis();
     }
 
-    private static void UpdateLoanUis() {
-        var loanItems = Object.FindObjectsOfType<LoanItem>();
-
-        foreach (var loanItem in loanItems) {
-            var method = ReflectionUtils.GetNonPublicMethod(loanItem, "UpdateAvailableLayoutUI");
-
-            method.Invoke(loanItem, Array.Empty<object>());
-        }
-    }
 }
