badbf0424a8cf1f233cdca1177efc76d0841472a
Qoqqi <sazykin.georgii@gmail.com>
2024-05-06 13:25:31 +0300

    refactor: Класс Loans.LoanInfo вынесен в отдельный файл Loans.LoanInfo.cs

---
+++ src/Modules/Loans.LoanInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace QMarketPlugin.Modules;
+
+public static partial class Loans {
+    private class LoanInfo {
+
+        public BankCreditSO Instance { get; private set; }
+
+        public readonly LoanSource Source;
+
+        public readonly string Title;
+
+        private readonly Action<BankCreditSO> Filler;
+
+        public LoanInfo(LoanSource source, string title, int sum, float dailyPercent, int requiredLevel) {
+            Source = source;
+            Title = title;
+            Filler = loan => {
+                loan.Amount = sum;
+                loan.DailyInterestPercent = dailyPercent;
+                loan.RequiredPlayerLevel = requiredLevel;
+            };
+        }
+
+        public void AssignInstance(BankCreditSO instance) {
+            Instance = instance;
+            Filler(instance);
+        }
+    }
+}
